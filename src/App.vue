d305fb79ffa575c39a9c433b5fd8124414e2f893
Iqbal Zayyan <73063960+iqbalzayn01@users.noreply.github.com>
2024-10-03 19:56:33 +0700

    update: first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+  <HelloWorld msg="Home!" class="text-3xl font-bold underline" />
+</template>
+
+<style scoped></style>
